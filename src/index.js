23435c56b9756cb21a68e1b83d22240fb6d1f418
isus-pisus <romchris01@gmail.com>
2018-02-18 19:58:32 -0500

    added react-router and bpptstrap

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 import 'core-js/fn/object/assign';
+import 'bootstrap/dist/css/bootstrap.css';
+import { BrowserRouter } from 'react-router-dom';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/Main';
 
 // Render the main component into the dom
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render((
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+), document.getElementById('app'));
